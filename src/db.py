1b17ae180c2e90cd22efe036ffbc861aee571161
nanjiang675-hub <nanjiang675@gmail.com>
2026-07-29 18:30:26 +0800

    Reject non-person faculty pages

--- src/db.py
+++ src/db.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 from .config import DB_PATH, ROOT
 
 
-DISCOVERY_DATA_VERSION = "7"
+DISCOVERY_DATA_VERSION = "8"
 
 
 class ClosingConnection(sqlite3.Connection):
